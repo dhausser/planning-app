18bf36da192d675e35649d8ac8935c929cae4b59
Davy Hausser <24432220+dhausser@users.noreply.github.com>
2019-03-04 22:15:41 +0100

    first commit

--- app.js
+++ app.js
@@ -20,11 +20,11 @@ require('./models/Holiday');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.get('/*', function(req, res) {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
 app.use('/', require('./routes/index'));
 
