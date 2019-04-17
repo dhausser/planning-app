c0a5292d1a47e33dcdd918cf16ab2ac2c43df0fe
Davy <24432220+dhausser@users.noreply.github.com>
2019-04-17 09:16:44 +0200

    roadmap is now dynamically loading epic childrens

--- client/src/pages/Roadmap.js
+++ client/src/pages/Roadmap.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
@@ -19,7 +19,7 @@ import { FilterContext } from '../context/FilterContext'
 import Filters from '../components/Filters'
 import { projectId } from '../credentials'
 
-const GET_ISSUES = gql`
+const GET_EPICS = gql`
   query issueList($jql: String, $pageSize: Int!) {
     issues(jql: $jql, pageSize: $pageSize) {
       startAt
@@ -40,72 +40,136 @@ const GET_ISSUES = gql`
   }
 `
 
+const GET_CHILDREN = gql`
+  query issueList($jql: String, $pageSize: Int!) {
+    issues(jql: $jql, pageSize: $pageSize) {
+      startAt
+      maxResults
+      total
+      issues {
+        id
+        key
+        summary
+        type
+        priority
+        status {
+          name
+          category
+        }
+        subtasks {
+          summary
+          type
+          priority
+          status {
+            name
+            category
+          }
+        }
+      }
+    }
+  }
+`
+
 export default function Roadmap() {
   const { fixVersion } = useContext(FilterContext)
-  const [epicChildren, setEpicChildren] = useState([])
+
   const epicsJql = `project = ${projectId} AND fixVersion = ${
     fixVersion.id
   } AND issuetype = epic`
+
   return (
-    <ContentWrapper>
-      <PageTitle>Roadmap</PageTitle>
-      <Filters />
-      <Query query={GET_ISSUES} variables={{ jql: epicsJql, pageSize: 10 }}>
-        {({ data, loading, error }) => {
-          if (loading) return <Spinner />
-          if (error)
-            return <EmptyState header="Fail" description={error.message} />
+    <Query query={GET_EPICS} variables={{ jql: epicsJql, pageSize: 100 }}>
+      {({
+        loading: loadingEpics,
+        error: errorEpics,
+        data: { issues: epics },
+      }) => {
+        if (loadingEpics) return <Spinner />
+        if (errorEpics)
+          return <EmptyState header="Fail" description={errorEpics.message} />
 
-          const {
-            issues: { issues },
-          } = data
-          if (issues.length) {
-            issues.map(issue => {
-              getEpicChildren(issue.id, fixVersion, setEpicChildren)
-              // issue.children = convertIssues(children)
-              return issue
-            })
-          }
+        return (
+          <Query
+            query={GET_CHILDREN}
+            variables={{
+              jql: `fixVersion = ${
+                fixVersion.id
+              } AND 'Epic Link' in (${epics.issues.map(({ id }) => id)})`,
+              pageSize: 25,
+            }}
+          >
+            {({
+              loading: loadingChildren,
+              error: errorChildren,
+              data: { issues: epicChildren },
+            }) => {
+              if (loadingChildren) return <Spinner />
+              if (errorChildren)
+                return (
+                  <EmptyState
+                    header="Fail"
+                    description={errorChildren.message}
+                  />
+                )
 
-          return (
-            <TableTree>
-              <Headers>
-                <Header width={150}>Type</Header>
-                <Header width={800}>Summary</Header>
-                <Header width={100}>Value</Header>
-                <Header width={200}>Status</Header>
-              </Headers>
-              <Rows
-                items={convertIssues(data.issues.issues)}
-                render={({ type, key, summary, value, status, children }) => (
-                  <Row
-                    expandLabel="Expand"
-                    collapseLabel="Collapse"
-                    itemId={key}
-                    items={children}
-                    hasChildren={children && children.length > 0}
-                  >
-                    <Cell singleLine>{type}</Cell>
-                    <Cell singleLine>
-                      {<Link to={`/issue/${key}`}>{summary}</Link>}
-                    </Cell>
-                    <Cell singleLine>{value}</Cell>
-                    <Cell singleLine>{status}</Cell>
-                  </Row>
-                )}
-              />
-            </TableTree>
-          )
-        }}
-      </Query>
-    </ContentWrapper>
+              if (epics.issues.length) {
+                epics.issues.forEach(issue => {
+                  issue.children = epicChildren.issues
+                })
+              }
+
+              return (
+                <ContentWrapper>
+                  <PageTitle>Roadmap</PageTitle>
+                  <Filters />
+                  <TableTree>
+                    <Headers>
+                      <Header width={150}>Type</Header>
+                      <Header width={800}>Summary</Header>
+                      <Header width={100}>Value</Header>
+                      <Header width={200}>Status</Header>
+                    </Headers>
+                    <Rows
+                      items={issueReducer(epics.issues)}
+                      render={({
+                        key,
+                        summary,
+                        type,
+                        value,
+                        status,
+                        children,
+                      }) => (
+                        <Row
+                          expandLabel="Expand"
+                          collapseLabel="Collapse"
+                          itemId={key}
+                          items={children}
+                          hasChildren={children && children.length > 0}
+                        >
+                          <Cell singleLine>{type}</Cell>
+                          <Cell singleLine>
+                            {<Link to={`/issue/${key}`}>{summary}</Link>}
+                          </Cell>
+                          <Cell singleLine>{value}</Cell>
+                          <Cell singleLine>{status}</Cell>
+                        </Row>
+                      )}
+                    />
+                  </TableTree>
+                </ContentWrapper>
+              )
+            }}
+          </Query>
+        )
+      }}
+    </Query>
   )
 }
 
-function convertIssues(issues) {
+function issueReducer(issues) {
   return issues.map(issue => ({
-    type: getIcon[issue.type],
     key: issue.key,
+    type: getIcon[issue.type],
     summary: issue.summary,
     value: getIcon[issue.priority],
     status: (
@@ -115,7 +179,6 @@ function convertIssues(issues) {
       issue.children &&
       issue.children.map(child => ({
         type: getIcon[child.type],
-        key: child.key,
         summary: child.summary,
         value: getIcon[child.priority],
         status: (
@@ -124,70 +187,18 @@ function convertIssues(issues) {
             color={getIcon[child.status.category]}
           />
         ),
-        children: child.fields.subtasks.map(subtask => ({
-          type: getIcon[subtask.fields.issuetype.name],
-          key: subtask.key,
-          summary: subtask.fields.summary,
-          value: getIcon[subtask.fields.priority.name],
+        children: child.subtasks.map(subtask => ({
+          type: getIcon[subtask.type],
+          summary: subtask.summary,
+          value: getIcon[subtask.priority],
           status: (
             <Status
-              text={subtask.fields.status.name}
-              color={getIcon[subtask.fields.status.statusCategory.key]}
+              text={subtask.status.name}
+              color={getIcon[subtask.status.category]}
             />
           ),
           children: [],
         })),
       })),
   }))
-}
-
-function getEpicChildren(epicIds, fixVersion, setEpicChildren) {
-  const jql = `project=${projectId} AND fixVersion = ${
-    fixVersion.id
-  } AND 'Epic Link' in (${epicIds})`
-
-  const GET_EPIC_CHILDREN = gql`
-    query issueList($jql: String, $pageSize: Int!) {
-      issues(jql: $jql, pageSize: $pageSize) {
-        startAt
-        maxResults
-        total
-        issues {
-          summary
-          type
-          priority
-          status {
-            name
-            category
-          }
-          subtasks {
-            summary
-            type
-            priority
-            status {
-              name
-              category
-            }
-          }
-        }
-      }
-    }
-  `
-
-  return (
-    <Query query={GET_EPIC_CHILDREN} variables={{ jql, pageSize: 10 }}>
-      {({ data, loading, error }) => {
-        if (loading) return <Spinner />
-        if (error)
-          return <EmptyState header="Error" description={error.message} />
-
-        const {
-          issues: { issues },
-        } = data
-        console.log(issues)
-        setEpicChildren(issues)
-        return null
-      }}
-    </Query>
-  )
 }
