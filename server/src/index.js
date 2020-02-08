b3a27d6620efde5989049aea8d251331523fa47d
Davy Hausser <24432220+dhausser@users.noreply.github.com>
2020-02-08 16:56:29 +0100

    script update

--- server/src/index.js
+++ server/src/index.js
@@ -1,3 +1,56 @@
-// eslint-disable-next-line no-global-assign
-require = require('esm')(module);
-module.exports = require('./main');
+import 'dotenv/config';
+import express from 'express';
+import session from 'express-session';
+import { ApolloServer } from 'apollo-server-express';
+
+import passport from './passport';
+import routes from './routes';
+import createStore from './db';
+import typeDefs from './schema';
+import resolvers from './resolvers';
+import IssueAPI from './datasources/issue';
+import AbsenceAPI from './datasources/absence';
+import ResourceAPI from './datasources/resource';
+
+/**
+ * Apollo Server
+ */
+const store = createStore();
+
+const apollo = new ApolloServer({
+  typeDefs,
+  resolvers,
+  formatError: (error) => {
+    console.error(error);
+    return error;
+  },
+  context: ({ req }) => ({
+    authorization: req.headers.authorization,
+    user: req.user,
+  }),
+  dataSources: () => ({
+    issueAPI: new IssueAPI(),
+    absenceAPI: new AbsenceAPI(),
+    resourceAPI: new ResourceAPI({ store }),
+  }),
+});
+
+const app = express();
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {},
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use('/', routes);
+
+apollo.applyMiddleware({ app });
+
+const port = process.env.NODE_ENV === 'production' ? 8080 : 4000;
+
+app.listen(port, () => console.log(
+  `Server ready at http://localhost:${port}${apollo.graphqlPath}`,
+));
