e0e007a392162b7ef9ee9ca9f531f32f197233f1
Davy Hausser <24432220+dhausser@users.noreply.github.com>
2020-02-29 21:27:45 +0100

    login

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,11 +1,28 @@
-import React, { Fragment } from 'react'
-import Router from "@reach/router";
-import LoginSuccess from './LoginSuccess'
-import LoginForm from "../components/LoginForm";
+import { useRouteMatch } from 'react-router-dom';
+import { useApolloClient, useMutation, gql } from '@apollo/client';
 
-export default () => (
-  <Router primary={false} component={Fragment}>
-    <LoginForm />
-    <LoginSuccess path="login" />
-  </Router>
-)
+const LOGIN_USER = gql`
+  mutation login {
+    login
+  }
+`;
+
+function Login() {
+  useRouteMatch('/login');
+  const client = useApolloClient();
+  const [login] = useMutation(
+    LOGIN_USER,
+    {
+      onCompleted: ({ login: token }) => {
+        localStorage.setItem('token', token);
+        client.writeData({ data: { isLoggedIn: true } });
+        window.opener.location.reload();
+        window.close();
+      },
+    },
+  );
+  login();
+  return null;
+}
+
+export default Login;
