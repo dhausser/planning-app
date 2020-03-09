0d3e37e649e3fcddd5dacc5d691a0a6be177d14d
Davy Hausser <davy.hausser@gmail.com>
2020-03-09 20:53:15 +0100

    typescript migration of filters complete

---
+++ client/src/components/Filters/VersionFilter.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import { ApolloClient, gql } from '@apollo/client';
+import Filter from './Filter';
+
+const GET_VERSIONS = gql`
+  query ($id: ID!) {
+    projectId @client @export(as: "id")
+    versionId @client
+    versions(id: $id) {
+      id
+      name
+    }
+  }
+`;
+
+function updateCache(client: ApolloClient<object >, value: string | null) {
+  client.writeQuery({
+    query: gql`{
+      versionId
+      statusId
+    }`,
+    data: {
+      versionId: value,
+      statusId: null,
+    },
+  });
+}
+
+export default () => (
+  <Filter
+    updateCache={updateCache}
+    query={GET_VERSIONS}
+    items="versions"
+    itemId="versionId"
+    isClearable
+  />
+);
