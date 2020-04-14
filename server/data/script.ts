6ae6dd97e3b90b1d161e837674ed48ed2dab7abb
Davy Hausser <davy.hausser@gmail.com>
2020-04-14 15:02:05 +0200

    prisma integration

---
+++ server/data/script.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from '@prisma/client';
+import resources from './resources.json';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  resources.forEach(async function insertData({
+    key,
+    email,
+    name,
+    team,
+    position,
+  }) {
+    await prisma.user.create({
+      data: {
+        key,
+        email,
+        name,
+        team,
+        position,
+      },
+    });
+  });
+  // const deleteUsers = await prisma.user.deleteMany({});
+  const allUsers = await prisma.user.findMany();
+  console.log(allUsers.length);
+}
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.disconnect();
+  });
