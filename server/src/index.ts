a6cc8e7c2b9794cf4301c53a334690558abf2a3a
Davy Hausser <davy.hausser@gmail.com>
2021-03-11 13:00:35 +0100

    fix server Oauth file missing error

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,23 +16,23 @@ import IssueAPI from './datasources/issue';
 import AbsenceAPI from './datasources/absence';
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 4000;
 const prisma = new PrismaClient();
 
-const RedisStore = connectRedis(session);
-const redisClient = redis.createClient({ password: process.env.REDIS_PWD });
+// const RedisStore = connectRedis(session);
+// const redisClient = redis.createClient({ password: process.env.REDIS_PWD });
 
 app.use(cookieParser());
 app.use(
   session({
-    store: new RedisStore({ client: redisClient }),
+    // store: new RedisStore({ client: redisClient }),
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: true,
   })
 );
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 app.use('/', routes);
 
 const apolloServer = new ApolloServer({
@@ -49,8 +49,8 @@ const apolloServer = new ApolloServer({
   }),
   dataSources: () => ({
     issueAPI: new IssueAPI({ prisma }),
-    absenceAPI: new AbsenceAPI(),
-    userAPI: new UserAPI({ prisma }),
+    // absenceAPI: new AbsenceAPI(),
+    // userAPI: new UserAPI({ prisma }),
   }),
 });
 
