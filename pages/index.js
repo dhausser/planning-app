1e80263e982803826f37d3bc15beae215c7e98ef
Davy Hausser <davy.hausser@gmail.com>
2021-03-12 15:09:32 +0100

    typescript added

--- pages/index.js
+++ pages/index.js
@@ -3,44 +3,36 @@ import Link from "next/link";
 import { useQuery } from "@apollo/client";
 import { initializeApollo } from "../apollo/client";
 
-const ViewerQuery = gql`
-  query ViewerQuery {
-    viewer {
-      id
-      name
-      status
-    }
-  }
-`;
+// const ViewerQuery = gql`
+//   query ViewerQuery {
+//     viewer {
+//       id
+//       name
+//       status
+//     }
+//   }
+// `;
 
 const Index = () => {
-  const {
-    data: { viewer },
-  } = useQuery(ViewerQuery);
+  // const {
+  //   data: { viewer },
+  // } = useQuery(ViewerQuery);
 
-  return (
-    <div>
-      You're signed in as {viewer.name} and you're {viewer.status} goto{" "}
-      <Link href="/about">
-        <a>static</a>
-      </Link>{" "}
-      page.
-    </div>
-  );
+  return <div>Hello world</div>;
 };
 
-export async function getStaticProps() {
-  const apolloClient = initializeApollo();
+// export async function getStaticProps() {
+//   const apolloClient = initializeApollo();
 
-  await apolloClient.query({
-    query: ViewerQuery,
-  });
+//   await apolloClient.query({
+//     query: ViewerQuery,
+//   });
 
-  return {
-    props: {
-      initialApolloState: apolloClient.cache.extract(),
-    },
-  };
-}
+//   return {
+//     props: {
+//       initialApolloState: apolloClient.cache.extract(),
+//     },
+//   };
+// }
 
 export default Index;
